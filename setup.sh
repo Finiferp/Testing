ce8bc71ede25ef5e377fb77c77dcc8371f32d0a4
Daniel <46568088+Finiferp@users.noreply.github.com>
2024-11-21 07:59:28 +0100

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,50 +1,61 @@
 #!/bin/bash
-# Andino IO setup script by Daniel Do Canto Batista @ MABU Concepts
-
+# Andino IO setup for POP script by Daniel Do Canto Batista @ MABU Concepts
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-println_green () {
+
+
+println_green (){
     printf "${GREEN}%s${NC}\n" "${1}"
 }
 
-println_red () {
+println_red (){
     printf "${RED}%s${NC}\n" "${1}"
 }
 
-println () {
+println (){
     printf "%s\n" "${1}"
 }
 
-# Ensure script is run in a writable directory
 if [ ! -w "${PWD}" ]; then
     println_red "Directory is not writable!"
-    exit 1
+    usage
 fi
 
 println_red "!!! Installation started !!!"
 
-# Update and upgrade
-println_green "Updating & upgrading packages..."
-sudo apt-get update && sudo apt-get upgrade -y
 
-# WiFi configuration
-println_green "Configuring WiFi..."
-println_green "Enter WiFi password:"
-read -s PASSWORD
-println_green "Enter WiFi interface (e.g., wlan0):"
+# update and upgrade
+println_green "Updating & upgrading packages..."
+
+sudo apt-get update
+sudo apt-get upgrade -y
+
+
+
+# wifi config
+println_green "Configuring wifi..."
+println_green "Configuring wifi password:"
+read PASSWORD
+println_green "Configuring wifi interface:"
 read INTERFACE
-sudo nmcli device wifi Hotspot ssid Sensora password "$PASSWORD" ifname "$INTERFACE"
+
+sudo nmcli device wifi Hotspot ssid Sensora password $PASSWORD ifname $INTERFACE
+
 sudo nmcli connection modify Hotspot connection.autoconnect yes
 
-# Install Mosquitto broker
-println_green "Installing Mosquitto broker..."
+
+
+# install mosquitto broker
+println_green "Installing mosquitto broker..."
+
 sudo apt-get install mosquitto mosquitto-clients -y
 
 println_green "Enter your MQTT username:"
 read USERNAME
-sudo mosquitto_passwd -c /etc/mosquitto/passwd "$USERNAME"
+
+mosquitto_passwd -c /etc/mosquitto/passwd $USERNAME
 
 println_green "Enter your MQTT username for Node-Red:"
 read NODEREDUSERNAME
@@ -52,32 +63,38 @@ println_green "Enter password for $NODEREDUSERNAME:"
 read -s NODEREDPASSWORD
 sudo mosquitto_passwd -b /etc/mosquitto/passwd "$NODEREDUSERNAME" "$NODEREDPASSWORD"
 
-# Configure ACL file
-ACL_FILE="/etc/mosquitto/acl"
-sudo tee "$ACL_FILE" > /dev/null <<EOF
-user $USERNAME
-topic readwrite #
 
-user $NODEREDUSERNAME
-topic read #
+ACL_FILE="/etc/mosquitto/acl"
+
+sudo tee "$ACL_FILE" > /dev/null <<EOF
+    user $USERNAME
+    topic readwrite #
+
+    user $NODEREDUSERNAME
+    topic read #
 EOF
 
 # Restart Mosquitto service
 if sudo systemctl restart mosquitto; then
     println_green "Mosquitto broker installed and configured successfully."
 else
-    println_red "Failed to restart Mosquitto. Please check your configuration."
+    echo "Failed to restart Mosquitto. Please check your configuration."
     exit 1
 fi
 
-# Install database
-println_green "Installing SQLite3 database..."
+sudo systemctl restart mosquitto
+
+# install Database
+println_green "Installing database..."
+
 sudo apt install sqlite3 -y
 
 DB_DIR="db"
 DB_NAME="smartMeter.db"
+
 sudo mkdir -p "$DB_DIR"
 sudo chmod 777 -R "$DB_DIR"
+
 DB_PATH="$DB_DIR/$DB_NAME"
 
 sqlite3 "$DB_PATH" <<EOF
@@ -97,64 +114,105 @@ CREATE TABLE smartmetergeneral(
 );
 EOF
 
-sudo chmod 777 "$DB_PATH"
+sudo chmod 777 $DB_PATH
+
 println_green "SQLite database and tables created successfully."
 
-# Install tcpdump
+
+
+# install tcpdump
 println_green "Installing tcpdump..."
-sudo apt-get install tcpdump -y
+
+sudo apt-get install tcpdump
+
+
 
 # SSH Certificate
 println_green "Generating SSH certificate..."
+
 println_green "Enter your Passphrase:"
 read PASSPHRASE
+
+ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N "$PASSPHRASE"
+
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
-ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N "$PASSPHRASE"
+
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
-println_green "SSH certificate generated successfully."
 
-# Install mbpoll
+println_green "SSH certificate Generated successfully."
+
+
+
+# Installing mbpoll
 println_green "Installing mbpoll..."
-wget -O- http://www.piduino.org/piduino-key.asc | sudo apt-key add -
-echo 'deb http://raspbian.piduino.org stretch piduino' | sudo tee /etc/apt/sources.list.d/piduino.list
+
+wget -O- http://www.piduino.org/piduino-key.asc | sudo apt-key add -echo 'deb http://raspbian.piduino.org stretch piduino' | sudo tee /etc/apt/
+
 sudo apt update
+
 sudo apt install mbpoll -y
 
-# Configure NTP server
-println_green "Configuring NTP server..."
+
+
+# configuring ntp server
+println_green "Configuring ntp server..."
+
 sudo apt install ntp -y
+
 CONFIG_FILE="/etc/ntpsec/ntp.conf"
 NTP_SERVER="194.154.216.81"
 
-if [[ -f "$CONFIG_FILE" ]]; then
-    sudo cp "$CONFIG_FILE" "${CONFIG_FILE}.bak"
-    sudo sed -i -e 's/^\(pool \|server \)/#&/g' "$CONFIG_FILE"
-    sudo sed -i '/^# Specify one or more NTP servers\./a server '"$NTP_SERVER"' iburst' "$CONFIG_FILE"
-    if sudo systemctl restart ntpsec; then
-        println_green "NTP server updated to $NTP_SERVER and service restarted successfully."
-    else
-        println_red "Failed to restart the NTP service. Please check the configuration."
-        exit 1
-    fi
+# Backup the original configuration file
+sudo cp "$CONFIG_FILE" "${CONFIG_FILE}.bak"
+
+# Check if the configuration file exists
+if [[ ! -f "$CONFIG_FILE" ]]; then
+    echo "Configuration file not found at $CONFIG_FILE. Exiting."
+    exit 1
+fi
+
+# Update the NTP configuration
+sudo sed -i -e 's/^\(pool \|server \)/#&/g' "$CONFIG_FILE"
+
+# Add the new NTP server line
+sudo sed -i '/^# Specify one or more NTP servers\./a server '"$NTP_SERVER"' iburst' "$CONFIG_FILE"
+
+# Restart the NTP service
+if sudo systemctl restart ntpsec; then
+    echo "NTP server updated to $NTP_SERVER and service restarted successfully."
 else
-    println_red "NTP configuration file not found at $CONFIG_FILE."
+    echo "Failed to restart the NTP service. Please check the configuration."
     exit 1
 fi
 
 # Configure IP Tables
 println_green "Configuring IP tables..."
-sudo apt install iptables -y
-sudo iptables -A FORWARD -i "$INTERFACE" -o eth0 -j DROP
+
+sudo apt install iptables
+
+iptables -A FORWARD -i $INTERFACE -o eth0 -j DROP
+
+
 
 # Andino setup
 println_green "Andino setup started..."
+
 wget 'https://raw.githubusercontent.com/andino-systems/andinopy/main/install_scripts/setup.sh'
+
 chmod +x setup.sh
+
 sudo ./setup.sh -m IO -n -s -o
+
 println_green "Installing sample & configuration flows..."
+
+#wget "https://raw.githubusercontent.com/andino-systems/Andino/master/Andino-Common/src/NodeRed/Flows/flows.json"
+#sudo cp flows.json /root/.node-red/flows.json
+#sudo rm flows.json
+
+
 
 println_green "!!! Installation completed !!!"
 println_green "Please copy the content of the file ~/.ssh to your the device."
-println_green "Run 'sudo reboot' to restart the system."
+println_green "Run 'sudo reboot' to restart the system"
